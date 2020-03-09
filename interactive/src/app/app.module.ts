6a2e0351c047a66d0df72c8f696bc91d458b0d0d
Brian Ramirez <brians@uninorte.edu.co>
2020-03-09 01:33:00 -0500

    chore: Add core and shared modules

--- interactive/src/app/app.module.ts
+++ interactive/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core';
+import { SharedModule } from './shared';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
-    AppRoutingModule
+
+    // Core & Shared
+    CoreModule,
+    SharedModule,
+
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
